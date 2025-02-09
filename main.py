e2b38334ff7e7930557a04e363775b445e94e45b
Melisa Ciftci <ciftcimelisa03@gmail.com>
2025-02-09 21:07:52 +0300

    Add new feature

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ while True:
     user_action= input("Type add, show, edit, complete or exit: ")
     user_action = user_action.strip()
 
-    if 'add' in user_action:
+    if 'add' in user_action or 'new' in user_action:
         todo=user_action[4:] # todo after 'add'. (slicing)
 
         '''file = open('todos.txt', 'r')
@@ -13,7 +13,7 @@ while True:
         with open('todos.txt', 'r') as file:  # : means indented
             todos = file.readlines()  # no need to close file.
 
-        todos.append(todo)
+        todos.append(todo + '\n')
 
         with open('todos.txt', 'w') as file:
             todos = file.writelines(todos)
